e6753799a4c0f8eaf7b62928b5e5d9d39454f5cd
Kaan Ozdemir <kaan.ozdemir@tsoft.com.tr>
2021-07-07 16:29:58 +0300

    .

---
+++ Framework B/Framework B/Framework_B.h
@@ -0,0 +1,18 @@
+//
+//  Framework_B.h
+//  Framework B
+//
+//  Created by Kaan Ozdemir on 7.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Framework_B.
+FOUNDATION_EXPORT double Framework_BVersionNumber;
+
+//! Project version string for Framework_B.
+FOUNDATION_EXPORT const unsigned char Framework_BVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework_B/PublicHeader.h>
+
+
